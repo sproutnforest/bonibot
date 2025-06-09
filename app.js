74a4bc704ae38e04b1c6fbadb47ab2fa8bb6a8a3
briggea <geasandhea19@gmail.com>
2025-06-09 18:00:40 +0700

    fixed some stuff

--- app.js
+++ app.js
@@ -3,26 +3,26 @@ const path = require('path');
 const app = express();
 const port = 5502;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'public', 'views', 'modelmenu.html'));
+    res.sendFile(path.join(__dirname,'Public', 'views', 'modelmenu.html'));
 })
 
 app.get('/modelj', (req, res) => {
-    res.sendFile(path.join(__dirname,'public', 'views', 'modelj.html'));
+    res.sendFile(path.join(__dirname,'Public', 'views', 'modelj.html'));
 });
 
 app.get('/modelk', (req, res) => {
-    res.sendFile(path.join(__dirname,'public', 'views', 'modelk.html'));
+    res.sendFile(path.join(__dirname,'Public', 'views', 'modelk.html'));
 })
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname,'public', 'views', 'register.html'));
+    res.sendFile(path.join(__dirname,'Public', 'views', 'register.html'));
 })
 
 app.get('/subjectmenu', (req, res) => {
-    res.sendFile(path.join(__dirname,'public', 'views', 'subjectmenu.html'));
+    res.sendFile(path.join(__dirname,'Public', 'views', 'subjectmenu.html'));
 })
 
 app.listen(port, () => {
